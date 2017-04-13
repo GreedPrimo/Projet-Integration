aa88ce8f4bc64d254d6eb92dd68cd87e559c6b57
Louis Chemisky <lchmsk69@gmail.com>
2017-04-13 23:24:14 +0200

    Revert "test Img erreur"
    This reverts commit bf8357d4ee4ef4d244aa6fd414ddd5c09f4853ed.

--- divayo-main/index.php
+++ divayo-main/index.php
@@ -22,13 +22,7 @@
 								?>
 								<article class=<?php echo "'style" . rand(1,6) . "'"; ?>>
 									<span class="image jimg-350">
-										<img src=<?php if ( empty ($row["imgArt1"]) ) 
-											{
-												echo "'" . (utf8_encode("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjgJGYMDLle3eIrq90fshGeXMpNeJXuJZJycHqszwAMOVPC-Nh")) . "'";
-											}
-										 	
-										 	 	echo "'" . (utf8_encode($row["imgArt1"])) . "'"; 
-										 	 										 	 ?> alt="" />
+										<img src=<?php echo "'" . (utf8_encode($row["imgArt1"])) . "'"; ?> alt="" />
 									</span>
 									<a href=<?php echo "'" . "product.php?product=". $row["codeArt"] . "'"; ?>>
 										<h2><?php echo(utf8_encode($row["designArt"])); ?></h2>
